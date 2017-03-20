99528f9b141baf73c09464b3fd250d3cae5366f1
AlexYakovis <yakovis@boylesoftware.com>
2017-03-20 14:02:55 -0400

    REALM SUPPORT

--- app/routes.js
+++ app/routes.js
@@ -43,14 +43,15 @@ module.exports = function (app, passport) {
         failureFlash: true
     }));
 
-    app.get('/auth/facebook', passport.authenticate('facebook', {session: false, scope: ['email']}));
 
-    // app.get('/auth/facebook/callback',
-    //     passport.authenticate('facebook', { session: false, failureRedirect: "/" }),
-    //     function(req, res) {
-    //         res.redirect("/profile?access_token=" + req.user.access_token);
-    //     }
-    // );
+
+
+    app.get('/auth/facebook',
+        passport.authenticate('facebook', {session: false, scope: ['email']}),
+        function (req, res) {
+            console.log(req.query)
+        }
+    );
 
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
@@ -61,6 +62,44 @@ module.exports = function (app, passport) {
             res.send(req.user)
         }
     );
+
+    // app.get('/auth/facebook/callback',
+    //     passport.authenticate('facebook', { session: false, failureRedirect: "/" }),
+    //     function(req, res) {
+    //         res.redirect("/profile?access_token=" + req.user.access_token);
+    //     }
+    // );
+
+
+    app.get('/auth/facebook/:realm', function (req, res, next) {
+        console.log('/auth/facebook/ realm=', req.params.realm);
+        passport.authenticate(
+            'facebook',
+            {
+                session: false,
+                scope: ['email'],
+                callbackURL: '/auth/facebook/callback/' + req.params.realm
+            }
+        )(req, res, next);
+    });
+
+
+    app.get('/auth/facebook/callback/:realm',
+        function (req, res, next) {
+            console.log('/auth/facebook/ realm=', req.params.realm);
+            passport.authenticate(
+                'facebook',
+                {
+                    callbackURL: "/auth/facebook/callback/" + req.params.realm,
+                    failureRedirect: '/',
+                    session: false
+                }
+            )(req, res, next)
+        },
+        function (req, res) {
+            console.log(req.params);
+            res.send(req.user)
+        });
 
 
 };
