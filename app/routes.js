906d90fcbda9d726f31c8a85dbc9a3bbb761e694
AlexYakovis <yakovis@boylesoftware.com>
2017-03-20 18:30:58 -0400

    add Google and params support (nw)

--- app/routes.js
+++ app/routes.js
@@ -5,18 +5,6 @@ module.exports = function (app, passport) {
         res.render('index.ejs');
     });
 
-    app.get('/profile', function (req, res) {
-
-        var flash = req.flash('loginMessage');
-        console.log('FLASH', flash);
-        if (req.isAuthenticated()) {
-            res.send(req.user);
-        } else {
-            res.status(401).send({message: 'Access Denied', Details: flash});
-        }
-    });
-
-
     app.get('/login',
         passport.authenticate('local-login', {
             session: false
@@ -35,61 +23,120 @@ module.exports = function (app, passport) {
             res.send(req.user)
         });
 
-
-    app.get('/auth/facebook',
-        passport.authenticate('facebook', {session: false, scope: ['email']}),
-        function (req, res) {
-            console.log(req.query)
-        }
-    );
-
-    app.get('/auth/facebook/callback',
-        passport.authenticate('facebook', {
-            session: false,
-            failureRedirect: '/'
-        }),
-        function (req, res) {
-            res.send(req.user)
-        }
-    );
-
+    //
+    // app.get('/auth/facebook',
+    //     passport.authenticate('facebook', {session: false, scope: ['email']})
+    // );
+    //
+    // app.get('/auth/google',
+    //     passport.authenticate('google', {session: false, scope: ['profile', 'email']})
+    // );
+    //
+    //
     // app.get('/auth/facebook/callback',
-    //     passport.authenticate('facebook', { session: false, failureRedirect: "/" }),
-    //     function(req, res) {
-    //         res.redirect("/profile?access_token=" + req.user.access_token);
+    //     passport.authenticate('facebook', {session: false, failureRedirect: '/'}),
+    //     function (req, res) {
+    //         res.send(req.user)
+    //     }
+    // );
+    //
+    //
+    // app.get('/auth/google/callback',
+    //     passport.authenticate('google', {session: false, failureRedirect: '/'}),
+    //     function (req, res) {
+    //         res.send(req.user)
     //     }
     // );
 
 
-    app.get('/auth/facebook/:realm', function (req, res, next) {
-        console.log('/auth/facebook/ realm=', req.params.realm);
+    // //REALM SUPPORT
+    //
+    // app.get('/auth/facebook/:realm', function (req, res, next) {
+    //     console.log('/auth/facebook/ realm=', req.params.realm);
+    //     passport.authenticate(
+    //         'facebook',
+    //         {
+    //             session: false,
+    //             scope: ['email'],
+    //             callbackURL: '/auth/facebook/callback/' + req.params.realm
+    //         }
+    //     )(req, res, next);
+    // });
+    //
+    // app.get('/auth/facebook/callback/:realm',
+    //     function (req, res, next) {
+    //         console.log('/auth/facebook/ realm=', req.params.realm);
+    //         passport.authenticate(
+    //             'facebook',
+    //             {
+    //                 callbackURL: "/auth/facebook/callback/" + req.params.realm,
+    //                 failureRedirect: '/',
+    //                 session: false
+    //             }
+    //         )(req, res, next)
+    //     },
+    //     function (req, res) {
+    //         console.log(req.params);
+    //         res.send(req.user)
+    //     });
+    //
+    //
+    // app.get('/auth/google/:realm', function (req, res, next) {
+    //     console.log('/auth/google/ realm=', req.params.realm);
+    //     passport.authenticate(
+    //         'google',
+    //         {
+    //             session: false,
+    //             scope: ['profile', 'email'],
+    //             callbackURL: '/auth/google/callback/' + req.params.realm
+    //         }
+    //     )(req, res, next);
+    // });
+    //
+    // app.get('/auth/google/callback/:realm',
+    //     function (req, res, next) {
+    //         console.log('/auth/google/ realm=', req.params.realm);
+    //         passport.authenticate(
+    //             'google',
+    //             {
+    //                 callbackURL: "/auth/google/callback/" + req.params.realm,
+    //                 failureRedirect: '/',
+    //                 session: false
+    //             }
+    //         )(req, res, next)
+    //     },
+    //     function (req, res) {
+    //         console.log(req.params);
+    //         res.send(req.user)
+    //     });
+
+
+    //params
+    app.get("/auth/facebook", function (req, res, next) {
+        console.log('---req.query.',req.query);
+
+        var callbackURL = '/auth/facebook/callback?realm=' + req.query.realm;
+        //var callbackURL = fbConfig.callbackURL + "?queryParams=" + req.query.queryParams;
         passport.authenticate(
-            'facebook',
-            {
-                session: false,
-                scope: ['email'],
-                callbackURL: '/auth/facebook/callback/' + req.params.realm
-            }
+            "facebook",
+            {session: false, scope: ['email'], callbackURL: callbackURL}
         )(req, res, next);
     });
 
 
-    app.get('/auth/facebook/callback/:realm',
-        function (req, res, next) {
-            console.log('/auth/facebook/ realm=', req.params.realm);
-            passport.authenticate(
-                'facebook',
-                {
-                    callbackURL: "/auth/facebook/callback/" + req.params.realm,
-                    failureRedirect: '/',
-                    session: false
-                }
-            )(req, res, next)
+    app.get("/auth/facebook/callback", function (req, res, next) {
+            console.log('-callback--req.query.',req.query);
+            var callbackURL = '/auth/facebook/callback?queryParams=' + req.query.realm;
+            //var callbackURL = fbConfig.callbackURL + "?queryParams=" + req.query.queryParams
+            passport.authenticate("facebook", {
+                callbackURL: callbackURL,
+                failureRedirect: "/login",
+                session: false
+            })(req, res, next)
         },
         function (req, res) {
-            console.log(req.params);
+            console.log(req.query.queryParams);
             res.send(req.user)
         });
-
 
 };
--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,14 @@
 module.exports = {
 	'facebookAuth' : {
+        "display": "popup",
         "clientID": "174774442988550",
         "clientSecret": "4b6cac8fb25a65923f459244035cb12d",
 		'callbackURL': '/auth/facebook/callback'
 	},
 
 	'googleAuth' : {
-		'clientID': 'enter client id here',
-		'clientSecret': 'enter client secret here',
-		'callbackURL': 'enter callback here'
+		'clientID': '135702057250-kvu4i7c7ps4o7noe1op3vfb0u7tieuvr.apps.googleusercontent.com',
+		'clientSecret': '2sBoZp55AI90ds8Rk90k5y0z',
+		'callbackURL': '/auth/google/callback'
 	}
 }
